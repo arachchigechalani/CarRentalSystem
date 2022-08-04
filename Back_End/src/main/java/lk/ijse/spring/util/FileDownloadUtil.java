427b33ad9ea5866ab60b91982a2d08fbcbffb1db
arachchigechalani <arachchigechalani@gmail.com>
2022-08-04 13:35:31 +0530

    updating customerServiceImpl

--- Back_End/src/main/java/lk/ijse/spring/util/FileDownloadUtil.java
+++ Back_End/src/main/java/lk/ijse/spring/util/FileDownloadUtil.java
@@ -3,6 +3,82 @@ package lk.ijse.spring.util;
 import lk.ijse.spring.dto.ImageDTO;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Component
+public class FileDownloadUtil {
+    private Path foundFile=null;
+
+
+    private void searchFile(Path path,String fileName){
+        foundFile=null;
+        try {
+            Files.list(path).forEach(file -> {
+                if (file.getFileName().toString().startsWith(fileName)){
+                    System.out.println("file  found");
+                    foundFile=file;
+                    return;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public Resource getFileAsResource(ImageDTO imageDTO) {
+
+        String pathDirectory = null;
+        Path path =null;
+        String imageName=imageDTO.getImageId()+imageDTO.getImageView()+".jpeg";
+
+        switch (imageDTO.getImageType()){
+
+            case "car" :  pathDirectory = "D:\\AAD\\Spring Project\\milestone 02 backend\\Easy_Car_Rental_System-Spring-Project-\\EasyCarRental\\src\\main\\resources\\static\\image\\CarImage";
+                path= Paths.get(pathDirectory);
+                searchFile(path,imageName);
+                break;
+
+            case "driverLicence" : pathDirectory = "D:\\AAD\\Spring Project\\milestone 02 backend\\Easy_Car_Rental_System-Spring-Project-\\EasyCarRental\\src\\main\\resources\\static\\image\\driversLicenceImage";
+                path= Paths.get(pathDirectory);
+                searchFile(path,imageName);
+                break;
+
+            case "idCard" :   pathDirectory = "D:\\AAD\\Spring Project\\milestone 02 backend\\Easy_Car_Rental_System-Spring-Project-\\EasyCarRental\\src\\main\\resources\\static\\image\\NICImage";
+                path= Paths.get(pathDirectory);
+                searchFile(path,imageName);
+                break;
+
+
+        }
+        try {
+            if (foundFile != null) {
+                return new UrlResource(foundFile.toUri());
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+}
+
+
+
+/*
+package lk.ijse.spring.util;
+
+import lk.ijse.spring.dto.ImageDTO;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -36,24 +112,27 @@ public class FileDownloadUtil {
 
         String pathDirectory = null;
         Path path = null;
+        String imageName = imageDTO.getImageId() + imageDTO.getImageView() + ".jpeg";
+
 
         switch (imageDTO.getImageType()) {
 
             case "car":
                 pathDirectory = "D:\\2 sem projects\\SpringCarRental_Front_Back\\CarRentalSystem\\Back_End\\src\\main\\resources\\static\\images\\CarImage";
                 path = Paths.get(pathDirectory);
-                String imageName = imageDTO.getImageId() + imageDTO.getImageView() + ".jpeg";
                 searchFile(path, imageName);
                 break;
 
             case "licence":
                 pathDirectory = "D:\\2 sem projects\\SpringCarRental_Front_Back\\CarRentalSystem\\Back_End\\src\\main\\resources\\static\\images\\LicenceImage";
                 path = Paths.get(pathDirectory + "/" + imageDTO.getImageId() + imageDTO.getImageType() + ".jpeg");
+                searchFile(path, imageName);
                 break;
 
             case "idCard":
                 pathDirectory = "D:\\2 sem projects\\SpringCarRental_Front_Back\\CarRentalSystem\\Back_End\\src\\main\\resources\\static\\images\\IDCardImage";
                 path = Paths.get(pathDirectory + "/" + imageDTO.getImageId() + imageDTO.getImageType() + ".jpeg");
+                searchFile(path, imageName);
                 break;
         }
 
@@ -69,4 +148,4 @@ public class FileDownloadUtil {
         return null;
 
     }
-}
+}*/
